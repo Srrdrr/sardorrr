8f16fa4aae820ffc4326721c7e569219fbd1c45e
Srrdrr <anvarovsardor926@gmail.com>
2025-03-12 21:25:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log('Saidazim Saidazim')
+// alert("Привет кизлар")
+
+
+var a = 5
+console.log(a)
+
+
+let nameK = "Kamron Chill"
+console.log(nameK)
+
+
+const ibo = 'Qovun bu ibo'
+// alert(ibo)
+
+let info = prompt('Ismingizni kiriting')
+console.log(info)
+
+
+let add = prompt('Familiyangizni kiriting')
+console.log(add)
+
+
+let age = prompt('Yoshingizni kiriting')
+console.log(age)
+
+
+let fg = prompt('Yoqtirgan faningizni yozing')
+console.log(fg)
+
+
+let tuman = prompt('Yashaydigan tumaningizni kiriting')
+console.log(tuman)
+
+alert(`Malumotlar kiritganiz uchun rahmat`)
+
